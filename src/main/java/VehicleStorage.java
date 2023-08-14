5b658ddb868f974eef7a4e33ef499356c1982173
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-08-14 18:12:53 -0500

    notes on Interface class

--- src/main/java/VehicleStorage.java
+++ src/main/java/VehicleStorage.java
@@ -1,5 +1,9 @@
 public interface VehicleStorage {
 
     /** Instance method */
+    /**
+     * An interface instance method does not need the `public` or `abstract` keywords... they are included
+     * automatically...
+     * */
     String storeStuff(String stuff);
 }
